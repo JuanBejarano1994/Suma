db325573dd51a39903ed4bd48684f0f8cd00f7e0
JuanBejarano1994 <juanbejarano94@outlook.com>
2020-02-13 09:13:13 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumaNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 0, n2 = 0, suma = 0;
+            Console.WriteLine("PROGRAMA SUMA ");
+            Console.Write("Ingrese Numero 1: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese Numero 2: ");
+            n2 = double.Parse(Console.ReadLine());
+            suma = n1 + n2;
+            Console.WriteLine("La Suma Es: "+suma);
+            Console.ReadKey();
+        }
+    }
+}
